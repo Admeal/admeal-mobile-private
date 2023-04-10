a2cc5863a0bd49f71f0f6250aa0b6f899035bdab
pountzas <nikos@pountzas.gr>
2023-04-11 00:34:49 +0300

    create home structure

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,17 @@
 import { Text, View, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import HomeTopBar from "../components/HomeTopBar";
+import RecipesBar from "../components/RecipesBar";
+import RecipeTabs from "../components/RecipeTabs";
 
 export default function Home() {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView className="relative flex-1 items-center justify-center bg-[#F4F4F4] px-8 ">
-      <TouchableOpacity className="">
-        <Text>start</Text>
-      </TouchableOpacity>
+      className="bg- [##E0E0E0] relative min-h-screen">
+      <HomeTopBar />
+      <RecipesBar />
+      <RecipeTabs />
     </SafeAreaView>
   );
 }
