4d008e48a7cde848b86d5e39874e0991d2929859
pountzas <nikos@pountzas.gr>
2023-04-12 12:36:17 +0300

    add RecipeTab component

--- components/RecipeTabs.tsx
+++ components/RecipeTabs.tsx
@@ -1,4 +1,13 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+  SectionList,
+  SafeAreaView
+} from "react-native";
+
 import { useState } from "react";
 import PopularCard from "./PopularCard";
 import TopEarningsCard from "./TopEarningsCard";
@@ -37,26 +46,63 @@ const RecipeTabs = () => {
       </View>
       <View>
         {!tabSwitch ? (
-          <View>
+          <ScrollView
+            // horizontal={false}
+            className="h-screen pb-40">
             <View>
               <Text className="text-lg font-bold">Popular Recipes</Text>
-              <View className="py-4">
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                className="flex-row  py-4">
                 <PopularCard />
+                <PopularCard recipeName="Fried chicken" />
+                <PopularCard recipeName="mousaka" />
+                <PopularCard recipeName="Fried chicken" />
+                <PopularCard recipeName="Fried chicken" />
+                <PopularCard />
+                <PopularCard recipeName="mousaka" />
+                <PopularCard recipeName="Fried chicken" />
+                <PopularCard recipeName="Fried chicken" />
+                <PopularCard recipeName="Fried chicken" />
+              </ScrollView>
+            </View>
+            <View className="pb-60">
+              <Text className="py-4 text-lg font-bold">Top Earnings</Text>
+              <View className="h-[100%] flex-row flex-wrap items-center justify-between pt-4">
+                <TopEarningsCard cookedCount={70} recipeName="Fried chicken" />
+                <TopEarningsCard cookedCount={70} recipeName="mousaka" />
+                <TopEarningsCard cookedCount={320} />
+                <TopEarningsCard cookedCount={970} recipeName="tzatziki" time={25} />
               </View>
             </View>
-            <View>
-              <Text className="text-lg font-bold">Top Earnings</Text>
-              <TopEarningsCard />
-            </View>
-          </View>
+          </ScrollView>
         ) : (
-          <>
+          <View className=" ">
             <View className="flex-row items-center space-x-2">
-              <Text>Total meals</Text>
+              <Text className="text-lg font-bold">Total meals</Text>
               <Text className="rounded-full bg-[#FFCAC2] p-1 text-[#BB1E09]">0</Text>
             </View>
-            <MyMealsCard />
-          </>
+            <View className="relative  pb-80">
+              <ScrollView
+                nestedScrollEnabled={true}
+                alwaysBounceVertical={true}
+                scrollEnabled={true}
+                className=" h-screen">
+                {/* className="mb-96 h-screen"> */}
+                <View onStartShouldSetResponder={() => true} className="pb-[500px]">
+                  <MyMealsCard status="Take a photo of the dish" />
+                  <MyMealsCard status="Take a photo of ingredients" />
+                  <MyMealsCard status="Finished" />
+                  <MyMealsCard status="Finished" />
+                  <MyMealsCard status="Finished" />
+                  <MyMealsCard status="Finished" />
+                  <MyMealsCard status="Finished" />
+                  <MyMealsCard status="Finished" />
+                </View>
+              </ScrollView>
+            </View>
+          </View>
         )}
       </View>
     </View>
