f8a875ee995dc273e0ee1479949ccb139ca9842c
pountzas <nikos@pountzas.gr>
2023-04-16 22:14:40 +0300

    setup fonts on on app root

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 import Home from "./screens/Home";
 import Sidebar from "./screens/Sidebar";
@@ -16,8 +19,29 @@ import MealsIcon from "./assets/icons/mealsIcon";
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-900": require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-800": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-700": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-600": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-500": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-400": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-300": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-200": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-100": require("./assets/fonts/Poppins-Thin.ttf")
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
       <NavigationContainer>
         <Drawer.Navigator
           initialRouteName="Recipes"
