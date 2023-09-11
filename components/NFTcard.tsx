e85d83045fb6f4f5a0a36b195192993b18f04dd4
pountzas <nikos@pountzas.gr>
2023-09-11 20:31:49 +0300

    testing nft card animation-video

--- components/NFTcard.tsx
+++ components/NFTcard.tsx
@@ -1,42 +1,70 @@
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
-
+import { Video, ResizeMode } from "expo-av";
 import PriceTag from "./PriceTag";
+import { useRef, useState } from "react";
 
-function NFTcard({
-  description = "lorem ipsum",
-  nft_name = "test",
-  recipe_images = ["https://picsum.photos/200/300"],
-  token_reward = 40
-}) {
+function NFTcard({ description = "lorem ipsum", nft_name = "test", token_reward = 40 }) {
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+
   const handleItemPress = () => {};
 
-  return (
-    <TouchableOpacity className="mr-2 shadow-2xl rounded-2xl" onPress={handleItemPress}>
-      <ImageBackground
-        style={[
-          {
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 5
-            },
-            shadowOpacity: 0.36,
-            shadowRadius: 5.68,
+  const getMedia = () => {
+    switch (nft_name) {
+      case "test":
+        return (
+          <ImageBackground
+            style={[
+              {
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 5
+                },
+                shadowOpacity: 0.36,
+                shadowRadius: 5.68,
 
-            elevation: 11
-          }
-        ]}
-        className=" h-[42vw] w-[42vw] flex-col justify-between rounded-2xl border border-[#919EAB] bg-black shadow-2xl"
-        borderRadius={16}
-        source={{
-          uri: recipe_images[0],
-          method: "POST"
-        }}>
-        <PriceTag tokenName="ADMEAL" price={token_reward} />
-      </ImageBackground>
+                elevation: 11
+              }
+            ]}
+            className=" h-[42vw] w-[42vw] flex-col justify-between rounded-2xl border border-[#919EAB] bg-black shadow-2xl"
+            borderRadius={16}
+            source={{
+              uri: "https://picsum.photos/200/300",
+              method: "POST"
+            }}>
+            <PriceTag tokenName="ADMEAL" price={token_reward} />
+          </ImageBackground>
+        );
+      case "Noodle NFT":
+        return (
+          <Video
+            ref={video}
+            source={{
+              uri: "https://firebasestorage.googleapis.com/v0/b/admeal-firebase.appspot.com/o/nft_media%2Fnoodle_nft.mp4?alt=media&token=35c08850-1a2b-42cd-bccb-44dc371d1b5f"
+            }}
+            className=" h-[42vw] w-[42vw] flex-col justify-between rounded-2xl border border-[#919EAB] bg-black shadow-2xl"
+            usePoster={true}
+            resizeMode={ResizeMode.COVER}
+            isLooping
+          />
+        );
+      case "ARNFT":
+        return "https://picsum.photos/200/300";
+      case "Admeal Romanesco":
+        return "https://picsum.photos/200/300";
+      default:
+        return "https://picsum.photos/200/300";
+    }
+  };
+
+  return (
+    <TouchableOpacity className="mr-2 rounded-2xl shadow-2xl" onPress={handleItemPress}>
       <View className="pt-2 pb-3">
         <Text className="font-[Poppins-600] text-sm text-[#212B36]">{nft_name}</Text>
-        <Text className="font-[Poppins-400] text-xs text-[#212B36]">{description}</Text>
+        <Text className="truncate font-[Poppins-400] text-xs text-[#212B36]">
+          {description}
+        </Text>
       </View>
     </TouchableOpacity>
   );
--- screens/Wallet.tsx
+++ screens/Wallet.tsx
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import * as Clipboard from "expo-clipboard";
 import { Motion } from "@legendapp/motion";
@@ -33,6 +33,8 @@ import { useRecoilState } from "recoil";
 import { userCreditsState, userState } from "../atoms/dataAtom";
 import firestore from "@react-native-firebase/firestore";
 
+import useNFTs from "../hooks/useNFTs";
+
 const Wallet = ({ navigation }: NavigationProp) => {
   const [userCredits, setUserCredits] = useRecoilState(userCreditsState);
   const [userItem, setUserItem] = useRecoilState(userState);
@@ -44,6 +46,12 @@ const Wallet = ({ navigation }: NavigationProp) => {
     useState<boolean>(false);
   const [isLogoutModalVisible, setIsLogoutModalVisible] = useState<boolean>(false);
   const [isMiniProfile, setIsMiniProfile] = useState<boolean>(false);
+  const [isfetchNfts, setIsfetchNfts] = useState<boolean>(false);
+  const [personalHarvestCollection, setPersonalHarvestCollection] = useState<any>(null);
+  const [personalNoodlesCollection, setPersonalNoodlesCollection] = useState<any>(null);
+  const [personalRamensCollection, setPersonalRamensCollection] = useState<any>(null);
+
+  const contractLoadingData = { _h: 0, _i: 0, _j: null, _k: null };
 
   useFocusEffect(
     useCallback(() => {
@@ -103,6 +111,87 @@ const Wallet = ({ navigation }: NavigationProp) => {
   const trancuateWalletAddress = () => {
     return `${address?.slice(0, 9)}...${address?.slice(-9)}`;
   };
+  const { getAllNFTs, getHarvestNFTs, getNoodleNFTs, getRamenNFTs } = useNFTs();
+
+  const harvestCollection = getHarvestNFTs();
+  const noodleCollection = getNoodleNFTs();
+  const ramenCollection = getRamenNFTs();
+
+  useEffect(() => {
+    harvestCollection.then((res) => {
+      personalHarvestCollection === null && setPersonalHarvestCollection(res);
+      console.log("res", res);
+      return res;
+    });
+  }, [harvestCollection]);
+
+  useEffect(() => {
+    noodleCollection.then((res) => {
+      personalNoodlesCollection === null && setPersonalNoodlesCollection(res);
+      console.log("res", res);
+      return res;
+    });
+  }, [noodleCollection]);
+
+  useEffect(() => {
+    ramenCollection.then((res) => {
+      personalRamensCollection === null && setPersonalRamensCollection(res);
+      console.log("res", res);
+      return res;
+    });
+  }, [ramenCollection]);
+
+  // console.log("harvest", harvestCollection);
+  // console.log("noodle", noodleCollection);
+  // console.log("ramen", ramenCollection);
+
+  // useEffect(() => {
+  //   if (harvestCollection !== undefined && personalHarvestCollection?.length === 0) {
+  //     harvestCollection &&
+  //       harvestCollection?.ownedNfts?.length &&
+  //       setPersonalHarvestCollection(harvestCollection?.ownedNfts);
+  //   }
+  //   console.log("harvest", harvestCollection);
+  // }, [harvestCollection]);
+
+  // useEffect(() => {
+  //   if (noodleCollection !== undefined && personalNoodlesCollection?.length === 0) {
+  //     noodleCollection &&
+  //       noodleCollection?.ownedNfts?.length &&
+  //       setPersonalNoodlesCollection(noodleCollection?.ownedNfts);
+  //   }
+  //   console.log("noodle", noodleCollection);
+  // }, [noodleCollection]);
+
+  // useEffect(() => {
+  //   if (ramenCollection !== undefined && personalRamensCollection?.length === 0) {
+  //     ramenCollection &&
+  //       ramenCollection?.ownedNfts?.length &&
+  //       setPersonalRamensCollection(ramenCollection?.ownedNfts);
+  //   }
+  //   console.log("ramen", ramenCollection);
+  // }, [ramenCollection]);
+
+  // harvestCollection?.then((res) => {
+  //   if (res.ownedNfts?.length && !personalHarvestCollection?.ownedNfts.length) {
+  //     setPersonalHarvestCollection(res.ownedNfts);
+  //     console.log("harvest", res);
+  //   }
+  // });
+
+  // noodleCollection?.then((res) => {
+  //   if (res.ownedNfts?.length && !personalNoodlesCollection?.ownedNfts.length) {
+  //     setPersonalNoodlesCollection(res.ownedNfts);
+  //     console.log("noodle", res);
+  //   }
+  // });
+
+  // ramenCollection?.then((res) => {
+  //   if (res.ownedNfts?.length && !personalRamensCollection?.ownedNfts.length) {
+  //     setPersonalRamensCollection(res.ownedNfts);
+  //     console.log("ramen", res);
+  //   }
+  // });
 
   const copyWalletAddress = async () => {
     await Clipboard.setStringAsync(address?.toString()!);
@@ -270,7 +359,22 @@ const Wallet = ({ navigation }: NavigationProp) => {
             NFTs
           </Text>
           <View className="relative flex-row flex-wrap items-center justify-between space-y-4 px-5">
-            <NFTcard />
+            <Text>{personalNoodlesCollection?.ownedNfts?.length} Noodles</Text>
+            {personalNoodlesCollection?.ownedNfts?.map((item: any, index: number) => (
+              <NFTcard key={index} description={item.description} nft_name={item.name} />
+            ))}
+
+            <Text>{personalRamensCollection?.ownedNfts?.length} Ramens</Text>
+
+            {personalRamensCollection?.ownedNfts?.map((item: any, index: number) => (
+              <NFTcard key={index} description={item.description} nft_name={item.name} />
+            ))}
+
+            <Text>{personalHarvestCollection?.ownedNfts?.length} Harvest</Text>
+            {personalHarvestCollection?.ownedNfts?.map((item: any, index: number) => (
+              <NFTcard key={index} description={item.description} nft_name={item.name} />
+            ))}
+
             <NFTcard />
             <NFTcard />
             <NFTcard />
