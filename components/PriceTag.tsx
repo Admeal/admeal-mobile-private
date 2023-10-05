63638f7a253cb731136c8e14df8f04658d2d84fd
pountzas <nikos@pountzas.gr>
2023-10-05 20:54:45 +0300

    memoize PriceTag component

--- components/PriceTag.tsx
+++ components/PriceTag.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { memo } from "react";
 import AdmealCoinLogo from "../assets/icons/admealCoinLogo";
 import DishCoinLogo from "../assets/icons/dishCoinLogo";
 
@@ -9,7 +9,7 @@ type PriceTagProps = {
   tokenName: string;
 };
 
-const PriceTag = ({ price, tokenName }: PriceTagProps) => {
+const PriceTag = memo(function PriceTag({ price, tokenName }: PriceTagProps) {
   return (
     <View className="flex-row items-center justify-end px-2 pt-2 ">
       <View className=" h-[21px] w-[55px] flex-row items-center justify-between rounded-xl bg-white px-[4px] pt-[1px]">
@@ -25,6 +25,6 @@ const PriceTag = ({ price, tokenName }: PriceTagProps) => {
       </View>
     </View>
   );
-};
+});
 
 export default PriceTag;
